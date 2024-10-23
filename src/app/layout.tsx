08ee0c9db36e67bae055e4f7f116940b2cecd4da
Jorge Viramontes <>
2024-10-23 16:43:19 -0500

    feat: set favicon to logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Lexend } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import Head from "next/head";
 
 const lexend = Lexend({ subsets: ["latin"] });
 
@@ -18,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/logo.svg" />
+      </Head>
       <body className={`min-h-screen ${lexend.className}`}>
         <NavBar />
         {children}
