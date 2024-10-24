52d0fa87d7c0787d9e2fc27548aeebf8f38fcb1d
galenom <galeno.mario@gmail.com>
2024-10-23 23:33:41 -0400

    refactor: add FullBrand component with svg and site title

---
+++ src/components/common/FullBrand/index.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export const FullBrand = () => {
+  return (
+    <>
+      <Image
+        src="/logo.svg"
+        width={40}
+        height={40}
+        alt="Logo for Latiné Professional Development Directory"
+      />
+      <p className="text-xl tracking-tighter text-white">LPDD</p>
+    </>
+  );
+};
