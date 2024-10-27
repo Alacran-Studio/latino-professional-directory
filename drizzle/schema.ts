44f9da62471d23c92016347dbbae08e97b619f83
Jorge Viramontes <>
2024-10-27 16:44:04 -0500

    feat: move schema back to drizzle folder, generate a new migration dropping industry, city, and region columns.

---
+++ drizzle/schema.ts
@@ -0,0 +1,21 @@
+import { integer, pgSchema, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const lpddSchema = pgSchema("lpdd");
+
+export const OrganizationsTable = lpddSchema.table("organizations", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  logo_url: text("logo_url"),
+  description: text("description"),
+  website_url: text("website_url").notNull(),
+});
+
+export const OrganizationContacts = lpddSchema.table("organization_contacts", {
+  id: serial("id").primaryKey(),
+  first_name: text("first_name").notNull(),
+  last_name: text("last_name").notNull(),
+  email: text("email").notNull().unique(),
+  organization_id: integer("organization_id").references(
+    () => OrganizationsTable.id
+  ),
+});
