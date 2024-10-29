41eabbc888ec2c17fe363145412cb9a1105d95fe
Jorge Viramontes <>
2024-10-29 16:16:24 -0500

    feat: add industry array to organizations in database.

--- src/app/types.ts
+++ src/app/types.ts
@@ -11,6 +11,12 @@ export interface DirectoryOrgType {
   logo_url: string;
   description: string;
   website_url: string;
+  industries: string[];
+}
+
+export interface IndustryType {
+  id: number;
+  name: string;
 }
 
 export enum Industry {
