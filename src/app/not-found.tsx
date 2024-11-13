6aea4edd31c5963fbe30b95bfeca405a18ded3a0
Roger Steve Ruiz <hi@rog.gr>
2024-11-13 16:43:02 -0600

    feat: Adding a 404 page;
    This is something that should be in-place for the `<OrganizationDetails/>` page.
    This is a placeholder for now. We can update it in later iterations.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+const title = "Something's missing.";
+const body =
+  "Sorry, we can't find that page. You'll find lots to explore on the home page.";
+
+export default function NotFound() {
+  return (
+    <section className="flex flex-row bg-white dark:bg-gray-900">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-7xl font-extrabold tracking-tight text-white lg:text-9xl">
+            404
+          </h1>
+          <p className="text-accent mb-4 text-3xl font-bold tracking-tight md:text-4xl">
+            {title}
+          </p>
+          <p className="mb-6 text-lg font-light text-gray-500 dark:text-gray-400">
+            {body}
+          </p>
+          <div className="py-10">
+            <Link
+              href="/"
+              className="rounded-xl bg-blue-500 px-8 py-4 text-center text-2xl text-white transition-all duration-200 hover:bg-blue-600"
+            >
+              Back to Homepage
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
