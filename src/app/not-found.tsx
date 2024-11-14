0eaa1b54cd2a669cc65e39f2d27a539c65ed5cb4
Jorge Viramontes <>
2024-11-14 15:13:33 -0600

    refactor: Add primary and primary-hover colors; fix typo with missing "-foreground" in IntroSection; extend NotFound component to take full screen height so component covers entire height; use existing LinkButton component for creating more robust common components.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import LinkButton from "@/components/common/LinkButton";
 import Link from "next/link";
 
 const title = "Something's missing.";
@@ -6,25 +7,20 @@ const body =
 
 export default function NotFound() {
   return (
-    <section className="flex flex-row bg-white dark:bg-gray-900">
+    <section className="flex h-full flex-row">
       <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
         <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-7xl font-extrabold tracking-tight text-white lg:text-9xl">
+          <h1 className="mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl">
             404
           </h1>
-          <p className="text-accent mb-4 text-3xl font-bold tracking-tight md:text-4xl">
+          <p className="mb-4 text-3xl font-bold tracking-tight text-accent md:text-4xl">
             {title}
           </p>
-          <p className="mb-6 text-lg font-light text-gray-500 dark:text-gray-400">
+          <p className="mb-6 text-lg font-light text-secondary-foreground">
             {body}
           </p>
           <div className="py-10">
-            <Link
-              href="/"
-              className="rounded-xl bg-blue-500 px-8 py-4 text-center text-2xl text-white transition-all duration-200 hover:bg-blue-600"
-            >
-              Back to Homepage
-            </Link>
+            <LinkButton href="/" title="Back to Homepage" />
           </div>
         </div>
       </div>
