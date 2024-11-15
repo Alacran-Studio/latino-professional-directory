ea4b13742bc6219e14b5d8c56b60053038936266
fgonza23 <fgonza0267@gmail.com>
2024-11-15 11:35:12 -0600

    Updated PR: Fixed additional dark mode styling issues

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -13,7 +13,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
         {...props}
         id={id}
         ref={ref}
-        className="h-12 rounded-md border-2 border-gray-300 pl-2 text-black dark:bg-gray-800 dark:text-white"
+        className="h-12 rounded-md border-2 border-gray-300 pl-2 dark:bg-gray-800"
       />
     );
 
@@ -21,10 +21,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
 
     return (
       <div className={`flex flex-col ${className}`}>
-        <label
-          htmlFor={id}
-          className="mb-2 font-bold text-black dark:text-white"
-        >
+        <label htmlFor={id} className="font-bolds mb-2">
           {label}
         </label>
         {textField}
