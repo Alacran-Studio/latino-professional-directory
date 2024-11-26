a248345efed613473952c5d0e91fe95fa0ebdb71
Jorge Viramontes <>
2024-11-26 13:38:26 -0600

    feat: remove duplicated code and create single className var with default shared classes, then setting specific classes separately; set width classes for directory so it fits nicely across different screens.

--- src/components/common/LinkButton/index.tsx
+++ src/components/common/LinkButton/index.tsx
@@ -11,13 +11,15 @@ export default function LinkButton({
   title,
   type = "default",
 }: ButtonProps) {
-  const buttonClasses =
+  let className =
+    "bg-primary text-sm text-white transition-all duration-200 hover:bg-primary-hover";
+  className +=
     type === "compact"
-      ? "rounded-lg bg-primary px-3 py-1 text-sm text-white transition-all duration-200 hover:bg-primary-hover"
-      : "rounded-xl bg-primary px-4 py-2 text-sm text-white transition-all duration-200 sm:px-8 sm:py-4 sm:text-2xl";
+      ? " rounded-lg px-3 py-1 sm:px-4 sm:py-2 sm:text-lg"
+      : " rounded-xl px-4 py-2 sm:px-8 sm:py-4 sm:text-2xl";
 
   return (
-    <Link href={href} className={buttonClasses}>
+    <Link href={href} className={className}>
       {title}
     </Link>
   );
