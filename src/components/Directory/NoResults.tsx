780e1714b1c464b848516cf1ee833b9595f20347
fgonza23 <fgonza0267@gmail.com>
2024-11-25 20:35:27 -0600

    feat: Implement compact button size option in LinkButton, update NoResults component

--- src/components/Directory/NoResults.tsx
+++ src/components/Directory/NoResults.tsx
@@ -9,15 +9,19 @@ export default function NoResults() {
       <Image
         src="/Search.svg"
         alt="No Results Found"
-        width={200}
-        height={200}
+        width={150}
+        height={150}
         className="opacity-75"
       />
       <h3 className="text-lg font-semibold">We couldn’t find any results.</h3>
       <p className="text-sm text-gray-500">
         Try a different search or refine your filters.
       </p>
-      <LinkButton href="/contact" title="Submit an Organization" />
+      <LinkButton
+        href="/contact"
+        title="Submit an Organization"
+        type="compact"
+      />
     </div>
   );
 }
--- src/components/common/LinkButton/index.tsx
+++ src/components/common/LinkButton/index.tsx
@@ -3,13 +3,21 @@ import Link from "next/link";
 interface ButtonProps {
   href: string;
   title: string;
+  type?: "compact" | "default";
 }
-export default function LinkButton({ href, title }: ButtonProps) {
+
+export default function LinkButton({
+  href,
+  title,
+  type = "default",
+}: ButtonProps) {
+  const buttonClasses =
+    type === "compact"
+      ? "rounded-lg bg-primary px-3 py-1 text-sm text-white transition-all duration-200 hover:bg-primary-hover"
+      : "rounded-xl bg-primary px-4 py-2 text-sm text-white transition-all duration-200 sm:px-8 sm:py-4 sm:text-2xl";
+
   return (
-    <Link
-      href={href}
-      className="hover:bg-primary-hover rounded-xl bg-primary px-4 py-2 text-sm text-white transition-all duration-200 sm:px-8 sm:py-4 sm:text-2xl"
-    >
+    <Link href={href} className={buttonClasses}>
       {title}
     </Link>
   );
